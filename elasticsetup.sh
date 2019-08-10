44a781b6c4fdfb8e154e99be10330f6e039eb49c
DrewTurner <drew@drewnturner.com>
2019-08-10 15:30:08 -0500

    Update elasticsetup.sh
    removing old oracle java integration and using built in with Elastic.

--- elasticsetup.sh
+++ elasticsetup.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:webupd8team/java -y
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
-sudo apt update && sudo apt install -y oracle-java8-set-default
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get update && sudo apt install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.l
 ist.d/elastic-7.x.list
-sudo apt-get update && sudo apt install apt-transport-https
-sudo apt install elasticsearch -y
+sudo apt-get update && sudo apt-get install elasticsearch
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
