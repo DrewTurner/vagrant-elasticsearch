4bec364d9a08d3d02b1c0bbd1ec548efbb73a0b4
DrewTurner <drew@drewnturner.com>
2019-08-10 15:13:59 -0500

    Update elasticsetup.sh
    upgrading from elastic 6 to 7

--- elasticsetup.sh
+++ elasticsetup.sh
@@ -7,8 +7,8 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo debconf-set-selections
 sudo apt update && sudo apt install -y oracle-java8-set-default
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.l
-ist.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.l
+ist.d/elastic-7.x.list
 sudo apt-get update && sudo apt install apt-transport-https
 sudo apt install elasticsearch -y
 sudo /bin/systemctl daemon-reload
