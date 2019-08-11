99e084dc7a536f522970b07d4af9c61e909247d9
DrewTurner <drew@drewnturner.com>
2019-08-11 10:14:53 -0500

    Update elasticsetup.sh

--- elasticsetup.sh
+++ elasticsetup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get update && sudo apt install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
+sudo apt update
+sudo apt install apt-transport-https -y
+sudo apt install elasticsearch -y
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
